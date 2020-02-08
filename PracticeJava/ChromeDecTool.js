bbf1bf983cde485562d82192a609eef7b552cbc8
dillonkiley1998 <dillonkiley1998@gmail.com>
2020-02-07 16:26:18 -0800

    node

---
+++ PracticeJava/ChromeDecTool.js
@@ -0,0 +1,30 @@
+/* 
+Input:
+12*3
+Output:
+36
+
+Clears console:
+console.clear(); 
+
+Input:
+var foo = 20 / 5;
+Output:
+"undefined"
+
+Input:
+var foo = 20 / 5;
+console.log(foo);
+
+Output:
+4
+
+Input:
+var foo = 20 / 5;
+console.log(foo);
+foo = false;
+
+Output:
+false
+
+*/
